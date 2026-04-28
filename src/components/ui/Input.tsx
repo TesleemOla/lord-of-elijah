fec677a08ef4aebd0a482e292a4eff0853bc9293
Tesleem Oladepo <oladepotesleem5@gmail.com>
2026-04-28 10:28:24 +0100

    placeholder text change

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-[var(--border)] bg-background/50 px-3 py-2 text-sm placeholder:text-slate-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all glass-panel",
+          "flex h-10 w-full rounded-md border border-[var(--border)] bg-background/50 px-3 py-2 text-sm placeholder:text-slate-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-all glass-panel",
           className
         )}
         ref={ref}
